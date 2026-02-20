eb4f9e3675b8db1d4e5a4d25d6930dc5944ec2fc
PadelMistral <juanaanvlc22@gmail.com>
2026-02-20 19:51:15 +0100

    Add files via upload

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,2 +1,1 @@
 ﻿importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-importScripts("./sw.js");
--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
 const CACHE_NAME = "padeluminatis-v7.2";
 const CORE_ASSETS = [
   "./",
